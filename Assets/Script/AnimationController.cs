a7539209cf1c70ce303e14d3f1b53a6d8f41bab8
agostinodati <agostino.dati@outlook.it>
2022-06-05 17:51:10 +0200

    charactermovement fixed

--- Assets/Script/AnimationController.cs
+++ Assets/Script/AnimationController.cs
@@ -7,12 +7,14 @@ public class AnimationController : MonoBehaviour
     private Animator animator;
     private MovementController movement;
     private CharacterMovement componentCharacterMovement;
+    private CombatAnimationcontroller combatController;
 
     // Start is called before the first frame update
     void Start()
     {
         animator = GetComponent<Animator>();
 
+        combatController = GetComponent<CombatAnimationcontroller>();
         componentCharacterMovement = GetComponentInParent<CharacterMovement>();
         movement = componentCharacterMovement.getMovement();
 
@@ -23,75 +25,76 @@ public class AnimationController : MonoBehaviour
     private void FixedUpdate()
     {
         Vector2 charMove = movement.Main.Move.ReadValue<Vector2>();
-
-        // If player move isWalking to true
-        if(charMove.y > 0 && charMove.x == 0)
+        if (!(combatController.getIsAttacking()))
         {
-            animator.SetBool("isWalkingForward", true);
-            animator.SetBool("isWalkingBackward", false);
-            animator.SetBool("isWalkingRight", false);
-            animator.SetBool("isWalkingLeft", false);
-            //Debug.Log("I'm running!");
+            // If player move isWalking to true
+            if (charMove.y > 0 && charMove.x == 0)
+            {
+                animator.SetBool("isWalkingForward", true);
+                animator.SetBool("isWalkingBackward", false);
+                animator.SetBool("isWalkingRight", false);
+                animator.SetBool("isWalkingLeft", false);
+                //Debug.Log("I'm running!");
+            }
+            else if (charMove.y < 0 && charMove.x == 0)
+            {
+                animator.SetBool("isWalkingBackward", true);
+                animator.SetBool("isWalkingForward", false);
+                animator.SetBool("isWalkingRight", false);
+                animator.SetBool("isWalkingLeft", false);
+                //Debug.Log("I'm running backward!");
+            }
+            else if (charMove.y == 0 && charMove.x > 0)
+            {
+                animator.SetBool("isWalkingForward", false);
+                animator.SetBool("isWalkingBackward", false);
+                animator.SetBool("isWalkingRight", true);
+                animator.SetBool("isWalkingLeft", false);
+            }
+            else if (charMove.y == 0 && charMove.x < 0)
+            {
+                animator.SetBool("isWalkingForward", false);
+                animator.SetBool("isWalkingBackward", false);
+                animator.SetBool("isWalkingLeft", true);
+                animator.SetBool("isWalkingRight", false);
+            }
+            else if (charMove.y < 0 && charMove.x < 0)
+            {
+                animator.SetBool("isWalkingForward", false);
+                animator.SetBool("isWalkingRight", false);
+                animator.SetBool("isWalkingBackward", true);
+                animator.SetBool("isWalkingLeft", true);
+            }
+            else if (charMove.y < 0 && charMove.x > 0)
+            {
+                animator.SetBool("isWalkingForward", false);
+                animator.SetBool("isWalkingLeft", false);
+                animator.SetBool("isWalkingBackward", true);
+                animator.SetBool("isWalkingRight", true);
+            }
+            else if (charMove.y > 0 && charMove.x < 0)
+            {
+                animator.SetBool("isWalkingRight", false);
+                animator.SetBool("isWalkingBackward", false);
+                animator.SetBool("isWalkingForward", true);
+                animator.SetBool("isWalkingLeft", true);
+            }
+            else if (charMove.y > 0 && charMove.x > 0)
+            {
+                animator.SetBool("isWalkingBackward", false);
+                animator.SetBool("isWalkingLeft", false);
+                animator.SetBool("isWalkingForward", true);
+                animator.SetBool("isWalkingRight", true);
+            }
+            else if (charMove.y == 0 && charMove.x == 0)
+            {
+                animator.SetBool("isWalkingForward", false);
+                animator.SetBool("isWalkingBackward", false);
+                animator.SetBool("isWalkingRight", false);
+                animator.SetBool("isWalkingLeft", false);
+                //Debug.Log("I'm chilling!");
+            }
         }
-        else if (charMove.y < 0 && charMove.x == 0)
-        {
-            animator.SetBool("isWalkingBackward", true);
-            animator.SetBool("isWalkingForward", false);
-            animator.SetBool("isWalkingRight", false);
-            animator.SetBool("isWalkingLeft", false);
-            //Debug.Log("I'm running backward!");
-        }
-        else if(charMove.y == 0 && charMove.x > 0)
-        {
-            animator.SetBool("isWalkingForward", false);
-            animator.SetBool("isWalkingBackward", false);
-            animator.SetBool("isWalkingRight", true);
-            animator.SetBool("isWalkingLeft", false);
-        }
-        else if (charMove.y == 0 && charMove.x < 0)
-        {
-            animator.SetBool("isWalkingForward", false);
-            animator.SetBool("isWalkingBackward", false);
-            animator.SetBool("isWalkingLeft", true);
-            animator.SetBool("isWalkingRight", false);
-        }
-        else if (charMove.y < 0 && charMove.x < 0)
-        {
-            animator.SetBool("isWalkingForward", false);
-            animator.SetBool("isWalkingRight", false);
-            animator.SetBool("isWalkingBackward", true);
-            animator.SetBool("isWalkingLeft", true);
-        }
-        else if (charMove.y < 0 && charMove.x > 0)
-        {
-            animator.SetBool("isWalkingForward", false);
-            animator.SetBool("isWalkingLeft", false);
-            animator.SetBool("isWalkingBackward", true);
-            animator.SetBool("isWalkingRight", true);
-        }
-        else if (charMove.y > 0 && charMove.x < 0)
-        {
-            animator.SetBool("isWalkingRight", false);
-            animator.SetBool("isWalkingBackward", false);
-            animator.SetBool("isWalkingForward", true);
-            animator.SetBool("isWalkingLeft", true);
-        }
-        else if (charMove.y > 0 && charMove.x > 0)
-        {
-            animator.SetBool("isWalkingBackward", false);
-            animator.SetBool("isWalkingLeft", false);
-            animator.SetBool("isWalkingForward", true);
-            animator.SetBool("isWalkingRight", true);
-        }
-        else if (charMove.y == 0 && charMove.x == 0)
-        {
-            animator.SetBool("isWalkingForward", false);
-            animator.SetBool("isWalkingBackward", false);
-            animator.SetBool("isWalkingRight", false);
-            animator.SetBool("isWalkingLeft", false);
-            //Debug.Log("I'm chilling!");
-        }
-        
 
     }
 }
