11b484759fa999ef98e04d12bfa842b010f8bdbd
ABTCoder <amalbenson98@gmail.com>
2022-06-07 15:32:58 +0200

    UI prototype

---
+++ Assets/Script/UI/HealthBar.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBar : MonoBehaviour
+{
+    [SerializeField] private Slider health;
+
+    private void Awake()
+    {
+        health = GetComponent<Slider>();
+    }
+
+    public void SetHealth(int maxHealth)
+    {
+        health.maxValue = maxHealth;
+        health.value = maxHealth;
+    }
+
+    public void TakeDamage(int damage)
+    {
+        health.value -= damage;
+    }
+}
